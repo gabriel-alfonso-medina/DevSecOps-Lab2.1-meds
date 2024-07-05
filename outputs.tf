6fcc4bdd975ff3336e8e01f3992340d52a732e15
EC2 Default User <ec2-user@ip-172-31-89-165.ec2.internal>
2024-07-05 04:06:21 +0000

    first commit

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "app_server_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = "i-0fc89a0c387c86c81"
+}
+
+output "app_server_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = "52.77.221.19"
+}
